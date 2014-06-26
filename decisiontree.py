9d2487b76688316ae97092e15ec55420efceae1e
Bemmu Sepponen <manga@bemmu.com>
2014-06-26 15:02:18 +0900

    Some output generated, but with strange one-child nodes

--- decisiontree.py
+++ decisiontree.py
@@ -1,4 +1,5 @@
 from equations import *
+from texttree import TextTree
 
 training_set = [
 	{'Hearing loss':'No', 'Injury':'No', 'Frequency of vertigo attacks':'0', 'classes' : {'not-BPV':3, 'BPV':0}},
@@ -13,6 +14,7 @@ training_set = [
 ]
 
 def find_best_attribute_to_split_on(training_set):
+	"""Which attribute has the highest information gain?"""
 	best_attr = None
 	best_gain = None
 	for attr in [key for key in training_set[0].keys() if key != 'classes']:
@@ -22,5 +24,48 @@ def find_best_attribute_to_split_on(training_set):
 			best_attr = attr
 	return best_attr
 
-print "Should split on attribute %s" % find_best_attribute_to_split_on(training_set)
+def unique_values(training_set, attribute):
+	"""Return all the different values that the given attribute has in the given training data"""
+	values = [x[attribute] for x in training_set]
+	return dict([(a, '') for a in values]).keys()
 
+def something(training_set, attr, value):
+
+	# Only include cases where attribute has the given value
+	training_set = [c for c in training_set if c[attr] == value]
+
+	# Remove the attribute from the cases
+	training_set = [dict([(a,v) for a,v in x.items() if a != attr]) for x in training_set]
+
+	return training_set
+
+# [
+#	{'classes': {'BPV': 0, 'not-BPV': 63}, 'Injury': 'No', 'Frequency of vertigo attacks': '0'}, 
+#   {'classes': {'BPV': 0, 'not-BPV': 28}, 'Injury': 'No', 'Frequency of vertigo attacks': '1'}, 
+#   {'classes': {'BPV': 0, 'not-BPV': 234}, 'Injury': 'No', 'Frequency of vertigo attacks': '2'}, 
+#   {'classes': {'BPV': 0, 'not-BPV': 1}, 'Frequency of vertigo attacks': '1'}, 
+#   {'classes': {'BPV': 0, 'not-BPV': 30}, 'Frequency of vertigo attacks': '2'}]
+
+def tree(training_set):
+	# The tree is the attribute to split on and subtrees have the different values of the attribute, under which are the next splits.
+	#
+	#  Attribute  
+	#    __|__      
+	#   |     |     
+	# Value  Value 
+	#   |     |
+	#   |     |
+	#  next  next
+ 	#  attr  attr
+	#  tree  tree
+	#
+   	attr = find_best_attribute_to_split_on(training_set)
+   	if attr is None:
+   		likeliest_class = sorted(training_set[0]['classes'].items(), cmp=lambda a,b:b[1]-a[1])[0][0]
+   		return (likeliest_class,)
+
+	values = sorted(unique_values(training_set, attr))
+	return (attr,) + tuple([(value,tree(something(training_set, attr, value))) for value in values])
+
+tt = TextTree(tree(training_set))
+print str(tt)
